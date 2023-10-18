0161e1ef3b4f52feb3bc9c6e7aa0be5648616a0a
ErikReis <3942018-ErikReis@users.noreply.replit.com>
2023-10-18 20:40:25 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,285 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const { Chess } = require('chess.js');
+const replitdbclient = require("@replit/database");
+const database = new replitdbclient();
+
+database.delete("nextRoomId");
+database.empty();
+
+
+/*
+await database.set("key", "value");
+let key = await database.get("key");
+console.log(key);
+*/
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+  pingInterval: 25000,
+  pingTimeout: 20000,
+});
+
+var nextRoomId = 0;
+
+database.get("nextRoomId").then((databaseNextRoomId) => {
+  if (databaseNextRoomId) {
+    nextRoomId = databaseNextRoomId;
+  } else {
+    database.set("nextRoomId", 0);
+    for (let i = 0; i < nextRoomId; i++) {
+      try {
+        database.delete('room-' + i);
+      } catch { }
+    }
+  }
+});
+
+
+const CLOCK_TIME = 1800;
+
+const androiddownloadlink = "https://drive.google.com/file/d/152-GotbvlDBNjJFW5505GW87NB1zXcW_/view?usp=drive_link";
+
+const most_recent_version = "0.5";
+
+app.use(express.static('public'));
+
+app.get('/downloadandroid', (req, res) => {
+  res.send(`<script>window.location.href = '${androiddownloadlink}';</script>`);
+});
+
+app.get('/room/:roomId', async (req, res) => {
+  if (rooms[req.params.roomId]) {
+    res.send(`<script>window.location.href = 'oxadrez://joinroom?id=${req.params.roomId}';window.location.href = '/downloadapp.html';</script>`);
+  } else {
+    let roompgn = await database.get('room-' + req.params.roomId);
+    if (roompgn) {
+      res.send(`<script>window.location.href = 'oxadrez://viewpgn?pgn=${encodeURIComponent(roompgn)}';window.location.href = '/downloadapp.html';</script>`);
+    }
+  }
+});
+
+
+app.get('/puzzle/:puzzleId', (req, res) => {
+  res.send(`<script>window.location.href = 'oxadrez://viewpuzzle?id=${req.params.puzzleId}';window.location.href = '/downloadapp.html';</script>`);
+});
+
+var rooms = {};
+
+var waitingrooms = {
+  "20": [],
+  "60": [],
+  "120": [],
+  "300": [],
+  "600": [],
+  "900": [],
+  "1800": [],
+  "3600": [],
+  "7200": [],
+};
+
+io.on('connection', socket => {
+
+  socket.on('createRoom', async () => {
+    try {
+      if (!rooms[nextRoomId]) {
+        rooms[nextRoomId] = { started: false, game: new Chess(), white: socket };
+        //rooms[nextRoomId].game.header('Event', 'Normal Chess Game', 'Site', 'https://oxadrez.com', 'Variant', 'Standart', 'TimeControl', `${CLOCK_TIME}+0`, 'White', `Usuário(1000)`, 'Black', `Usuário(1000)`);
+        socket.emit('roomCreated', nextRoomId);
+
+        nextRoomId += 1;
+        await database.set("nextRoomId", nextRoomId);
+      }
+    } catch {
+
+    }
+  });
+
+  socket.on('disconnect', (reason) => {
+    Object.keys(waitingrooms).forEach((waitingroom) => {
+      if (waitingrooms[waitingroom].includes(socket)) {
+        waitingrooms[waitingroom].splice(waitingrooms[waitingroom].indexOf(socket), 1);
+      }
+    })
+    try {
+
+      Object.keys(rooms).forEach((roomId) => {
+        if (rooms[roomId].black && rooms[roomId].black.id == socket.id) {
+          try {
+            if (rooms[roomId].white) {
+              rooms[roomId].white.emit('gameOver');
+            }
+            if (rooms[roomId].black) {
+              rooms[roomId].black.emit('gameOver');
+            }
+          } catch { }
+
+          database.set(`room-${roomId}`, rooms[roomId].game.pgn());
+          delete rooms[roomId];
+        }
+        if (rooms[roomId].white && rooms[roomId].white.id == socket.id) {
+          try {
+            if (rooms[roomId].white) {
+              rooms[roomId].white.emit('gameOver');
+            }
+            if (rooms[roomId].black) {
+              rooms[roomId].black.emit('gameOver');
+            }
+          } catch { }
+
+          database.set(`room-${roomId}`, rooms[roomId].game.pgn());
+          delete rooms[roomId];
+
+        }
+      });
+    } catch { }
+  });
+
+  socket.on('checkupdate', (app_version) => {
+    if (app_version == most_recent_version) {
+      socket.emit('updated');
+    } else {
+      socket.emit('not updated');
+    }
+  });
+
+  socket.on('quickMatch', async (selectedtime) => {
+    if (!selectedtime || !waitingrooms[selectedtime.toString()]) {
+      selectedtime = 1800;
+    }
+    try {
+      if (waitingrooms[selectedtime.toString()]) {
+        waitingrooms[selectedtime.toString()].push(socket);
+        while (waitingrooms[selectedtime.toString()].length >= 2) {
+          roomId = nextRoomId;
+          rooms[roomId] = { started: true, game: new Chess(), white: waitingrooms[selectedtime.toString()][0], black: waitingrooms[selectedtime.toString()][1], whiteTime: selectedtime, blackTime: selectedtime };
+          if (Math.random() < 0.5) {
+            rooms[roomId] = { started: true, game: new Chess(), white: waitingrooms[selectedtime.toString()][0], black: waitingrooms[selectedtime.toString()][1], whiteTime: selectedtime, blackTime: selectedtime };
+          }
+          //rooms[nextRoomId].game.header('Event', 'Normal Chess Game', 'Site', 'https://oxadrez.com', 'Variant', 'Standart', 'TimeControl', `${selectedtime}+0`, 'White', `Usuário(1000)`, 'Black', `Usuário(1000)`);
+          rooms[roomId].white.emit('joinPermitted', roomId, 'b', selectedtime, selectedtime);
+          rooms[roomId].black.emit('joinPermitted', roomId, 'w', selectedtime, selectedtime);
+
+          nextRoomId += 1;
+          await database.set("nextRoomId", nextRoomId);
+
+
+          waitingrooms[selectedtime.toString()].splice(0, 2);
+        }
+      }
+    } catch { }
+  });
+
+  socket.on('joinRoom', (roomId) => {
+    if (!roomId || !rooms[roomId]) {
+      rooms[nextRoomId] = { started: false, game: new Chess(), white: socket };
+      socket.emit('roomCreated', nextRoomId);
+
+      nextRoomId += 1;
+      database.set("nextRoomId", nextRoomId);
+    } else {
+      rooms[roomId].black = socket;
+      if (Math.random() < 0.5) {
+        const whitetemp = rooms[roomId].white;
+        const blacktemp = rooms[roomId].black;
+        rooms[roomId].white = blacktemp;
+        rooms[roomId].black = whitetemp;
+        rooms[roomId].whiteTime = CLOCK_TIME;
+        rooms[roomId].blackTime = CLOCK_TIME;
+      }
+      if (rooms[roomId]) {
+        rooms[roomId].white.emit('joinPermitted', roomId, 'b', CLOCK_TIME, CLOCK_TIME);
+        rooms[roomId].black.emit('joinPermitted', roomId, 'w', CLOCK_TIME, CLOCK_TIME);
+        rooms[roomId].started = true;
+      } else {
+        socket.emit('joinDenied', roomId);
+      }
+    }
+  });
+
+  socket.on('makeMove', ({ room, move }) => {
+    try {
+
+      if (!rooms[room] || !rooms[room].started) {
+
+      } else {
+        if (rooms[room].game.turn() == 'w' && rooms[room].white.id == socket.id) return;
+        if (rooms[room].game.turn() == 'b' && rooms[room].black.id == socket.id) return;
+
+        const result = rooms[room].game.move(move);
+        //rooms[room].game.setComment(`[% clk ${(rooms[room].game.turn() == 'w' ? rooms[room].blackTime : rooms[room].whiteTime)}]`);
+
+        if (result) {
+
+          rooms[room].white.emit('moveMade', move, rooms[room].whiteTime, rooms[room].blackTime);
+          rooms[room].black.emit('moveMade', move, rooms[room].blackTime, rooms[room].whiteTime);
+
+          if (rooms[room].game.isGameOver()) {
+            rooms[room].white.emit('gameOver');
+            rooms[room].black.emit('gameOver');
+            database.set(`room-${roomId} `, rooms[roomId].game.pgn());
+            delete rooms[roomId];
+          }
+        }
+      }
+    } catch {
+
+    }
+  });
+
+});
+
+
+setInterval(() => {
+  Object.keys(rooms).forEach((roomId) => {
+    if (rooms[roomId].started) {
+      if (rooms[roomId].game.turn() == "b") {
+        rooms[roomId].whiteTime -= 0.1;
+        if (rooms[roomId].whiteTime <= 0) {
+          rooms[roomId].white.emit('timeOver', "b");
+          rooms[roomId].black.emit('timeOver', "b");
+
+          database.set(`room-${roomId} `, rooms[roomId].game.pgn());
+          delete rooms[roomId];
+        }
+      } else {
+        rooms[roomId].blackTime -= 0.1;
+        if (rooms[roomId].blackTime <= 0) {
+          rooms[roomId].white.emit('timeOver', "w");
+          rooms[roomId].black.emit('timeOver', "w");
+
+
+          database.set(`room-${roomId} `, rooms[roomId].game.pgn());
+          delete rooms[roomId];
+        }
+      }
+    }
+  });
+}, 100);
+
+server.listen(3000);
+
+/* frontend js
+
+
+function createRoom() {
+    socket.emit('createRoom');
+    socket.on('roomCreated', room => {
+        roomId = room;
+        board = Chessboard2('board', config);
+        board.start();
+    });
+}
+
+function joinRoom(room) {
+    socket.emit('joinRoom', room);
+}
+
+
+*/
---
+++ public/chesslogic.js
@@ -0,0 +1,259 @@
+var selectedpremove = null;
+var gamestarted = false;
+var yourturn = 'w';
+var promotion = 'q';
+let board, game = null;
+var pendingMove = null;
+var pieceTheme = "lichess";
+var localStorage = window.localStorage;
+
+if (localStorage.getItem('pieceTheme')) {
+    pieceTheme = localStorage.getItem('pieceTheme');
+}
+
+var pendingPromotionMove = {};
+game = new Chess();
+
+document.body.innerHTML += `<link rel="stylesheet" href="themes/${pieceTheme}.css">`;
+
+function completePendingPromotionMove() {
+    const moveResult = game.move({
+        from: pendingPromotionMove.from,
+        to: pendingPromotionMove.to,
+        promotion: promotion
+    });
+
+    // was this a legal move?
+    if (moveResult) {
+        game.undo();
+        // update to the new position
+        board.set({
+            fen: game.fen()
+        });
+
+        onUserMoveEvent(moveResult);
+
+        updateStatus();
+        document.getElementById('promotion').style.display = "none";
+    }
+}
+
+// Default Chessground config 
+const chessgroundconfig = {
+
+    fen: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1', // starting FEN
+
+    orientation: 'white', // board orientation
+
+    coordinates: true, // show ranks and files
+
+    autoCastle: true, // automatically castle when king/rook move
+
+    viewOnly: false, // restrict premoves, clicks, drag pieces
+
+    disableContextMenu: false, // disable right click
+
+    resizable: true, // allow resize
+
+    animation: {
+        enabled: true,
+        duration: 200
+    }, 
+
+    movable: {
+        free: false, // all moves are valid - no validation
+        color: 'white', // allow both whites and blacks to move
+        showDests: true, // highlight destinations
+        dests: getDests(),
+        showDests: true,
+        rookCastle: true
+    },
+
+    drawable: {
+        enabled: true,
+        visible: true,
+    },
+
+    premovable: {
+        enabled: true, // allow premoves for both colors
+        events: {
+            set: function(orig, dest) {
+                selectedpremove = {
+                    from: orig,
+                    to: dest,
+                    promotion: promotion
+                };
+            },
+            unset: function() {
+                selectedpremove = null;
+            }
+        }
+    },
+
+    predroppable: {
+        enabled: false // disable predrops
+    },
+
+    highlight: {
+        lastMove: true, // highlight last move
+        check: true // highlight checks
+    },
+
+    events: {
+        move: (orig, dest) => {
+            const moveResult = game.move({
+                from: orig,
+                to: dest,
+                promotion: promotion
+            });
+
+            // was this a legal move?
+            if (moveResult) {
+                if (moveResult.san.includes('=')) {
+                    pendingPromotionMove = {
+                        from: orig,
+                        to: dest,
+                    }
+                    game.undo();
+                    document.getElementById('promotion').style.display = "block";
+                }else{
+                    game.undo();
+                    onUserMoveEvent(moveResult);
+                }
+            } else {
+                board.set({
+                    fen: game.fen()
+                });
+            }
+        },
+        dropNewPiece: (role, key) => {
+            // called when a new piece is dropped from pocket
+            // add to position
+        }
+    }
+
+};
+
+board = Chessground(document.getElementById('board'), chessgroundconfig);
+
+board.set({
+    fen: game.fen()
+});
+
+function getDests() {
+    var moves = new Map();
+    var letters = 'abcdefgh';
+    letters.split("").forEach((letter) => {
+        for (let number = 1; number <= 8; number++) {
+            let legalMoves = game.moves({ square: (letter + number), verbose: true });
+            let legalMovesFormatted = [];
+            legalMoves.forEach(legalMove => {
+                legalMovesFormatted.push(legalMove.to);
+            });
+            moves.set((letter + number), legalMovesFormatted);
+        }
+    });
+    return moves;
+}
+
+board = Chessground(document.getElementById('board'), chessgroundconfig);
+game = new Chess();
+
+function moveMade(move) {
+
+    let moveinfo = game.move(move);
+
+    if (moveinfo) {
+        if (yourturn == 'b') {
+            board.set({
+                orientation: "black"
+            });
+        }
+        board.move(moveinfo.from, moveinfo.to);
+        //board.set({lastMove: [moveinfo.from, moveinfo.to]});
+        board.set({
+            turnColor: (game.turn() == "w" ? 'white' : 'black'),
+            movable: {
+                free: false, // all moves are valid - no validation
+                color: (yourturn == "w" ? 'white' : 'black'), // allow both whites and blacks to move
+                showDests: true, // highlight destinations
+                dests: getDests(),
+                showDests: true,
+            },
+        });
+
+        if (moveinfo.san.includes('x')) {
+            let element = document.getElementById('piececaptured' + (game.turn() == "w" ? "1" : "0"));
+            element.innerHTML = `${element.innerHTML}<img width="25" height="25" src="img/chesspieces/${pieceTheme}/${game.turn()}${moveinfo.captured.toUpperCase()}.png">`
+        }
+    }
+    setInterval(() => {
+        board.set({
+            turnColor: (game.turn() == "w" ? 'white' : 'black'),
+        });
+    }, 3000);
+
+    //board.set({check: (game.isCheck() ? (game.turn() == "w" ? 'white' : 'black') : false)});
+
+    if (game.turn() == yourturn) {
+        if (selectedpremove) {
+            const moveinfo2 = game.move(selectedpremove);
+            if (moveinfo2) {
+                onUserMoveEvent(moveinfo2);
+                game.undo();
+            }
+            selectedpremove = null;
+        }
+    }
+    board.set({
+        fen: game.fen()
+    });
+
+
+    updateStatus();
+};
+
+function updateStatus() {
+    let status = 'Seu turno';
+
+    if (game.turn() !== yourturn) {
+        status = "Turno do oponente";
+    }
+
+    document.getElementById('status').innerHTML = status;
+}
+
+function gameOver() {
+
+    let winner = 'Empate';
+
+    if (game.isCheckmate()) {
+        winner = game.turn() === 'w' ? 'Vitória das pretas' : 'Vitória das Brancas'; 
+    }
+
+    document.getElementById('status').innerHTML = winner + `<button onclick="window.location.href = window.location.href;">RECARREGAR</button>`;
+
+    localStorage.setItem('lastGamePgn', game.pgn());
+
+
+}
+function StartGame(turn) {
+    game = new Chess();
+    board = Chessground(document.getElementById('board'), chessgroundconfig);
+    gamestarted = true;
+    yourturn = turn;
+    if (yourturn == 'b') {
+        board.set({
+            movable: {
+                color: 'black'
+            },
+            orientation: "black"
+        });
+    }
+    document.getElementById('promotion').innerHTML = `
+    <button onclick="promotion = 'q'; completePendingPromotionMove();"><img src="img/chesspieces/${pieceTheme}/${yourturn}Q.png" style="width: 50%; height: 50%;"></button>
+    <button onclick="promotion = 'r'; completePendingPromotionMove();"><img src="img/chesspieces/${pieceTheme}/${yourturn}R.png" style="width: 50%; height: 50%;"></button>
+    <button onclick="promotion = 'b'; completePendingPromotionMove();"><img src="img/chesspieces/${pieceTheme}/${yourturn}B.png" style="width: 50%; height: 50%;"></button>
+    <button onclick="promotion = 'n'; completePendingPromotionMove();"><img src="img/chesspieces/${pieceTheme}/${yourturn}N.png" style="width: 50%; height: 50%;"></button>`;
+    updateStatus();
+}
